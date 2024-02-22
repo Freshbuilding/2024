199d0c84c4f80f5ba47aa8f4f3cc7c3827b621e3
Freshbuilding <vincentfer66@gmail.com>
2024-02-22 18:10:54 +0100

    Update requirements.txt and fix preprocessing object file name

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -33,7 +33,7 @@ class DataTransformationConfig:
         This object is responsible for performing all preprocessing
         steps required before feeding data into a machine learning model.
     """
-    preprocessor_obj_file_path: str = os.path.join('artifacts', "proprocessor.pkl")
+    preprocessor_obj_file_path: str = os.path.join('artifacts', "preprocessor.pkl")
 
 
 class DataTransformation:
@@ -133,7 +133,7 @@ class DataTransformation:
             input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
             target_feature_test_df = test_df[target_column_name]
 
-            logging.info("Applying preprocessing object on training dataframe and testing dataframe.")
+            logging.info("Applying preprocessing object on training dataframe and testing dataframe")
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
@@ -143,7 +143,7 @@ class DataTransformation:
             ]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-            logging.info("Saved preprocessing object.")
+            logging.info("Saved preprocessing object")
 
             save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import pickle
+import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
@@ -16,7 +16,7 @@ def save_object(file_path, obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(str(e), sys.exc_info())
 
@@ -56,6 +56,6 @@ def load_object(file_path):
     """
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(str(e), sys.exc_info())
